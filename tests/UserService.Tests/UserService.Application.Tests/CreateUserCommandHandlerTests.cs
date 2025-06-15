d5d1ef188c49d9b1656e9e5fe3748aeb580a7583
francis <fad16papa@gmail.com>
2025-06-15 13:08:20 +0800

    Addid test cases for UserService

---
+++ tests/UserService.Tests/UserService.Application.Tests/CreateUserCommandHandlerTests.cs
@@ -0,0 +1,14 @@
+﻿using Moq;
+
+namespace UserService.Application.Tests;
+
+public class CreateUserCommandHandlerTests
+{
+    [Fact]
+    public void Handle_ShouldCreateUser_WhenDataIsValid()
+    {
+        //Arrange
+        // var userRepMock = new Mock<IUserService>();
+        // var handler = new CreateUserCommandHandler(userRepMock.Object);
+    }
+}
